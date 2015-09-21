517602a2833a6cc05273f45366ba097eae8d799c
smithdamen <smith.damen@gmail.com>
2015-09-21 14:50:32 -0400

    modified index

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+  //fitText script for title sections
+  $(".fitText").fitText(1.2, {
+    minFontSize: '20px',
+    maxFontSize: '40px'
+  });
+  
+  $(".fitText-main").fitText(1.2, {
+    minFontSize: '20px',
+    maxFontSize: '60px'
+  });
+});
